d770b13b88146808525d2813905e656f88b37ec8
sumantha123 <cslab0046@gmail.com>
2025-07-23 12:13:02 +0530

    Add files via upload

---
+++ jj.py
@@ -0,0 +1,11 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+sum_result = a + b
+difference = a - b
+product = a * b
+quotient = a / b
+modulus = a % b
+exponent = a ** b
+floor_division = a // b
+
+
